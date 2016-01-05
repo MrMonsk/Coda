476701292d4900316547873e239eb137f8f1f477
Yowon Yoon <yowonyoon@gmail.com>
2016-01-05 03:39:43 +0000

    tryna fix email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'yowonyoon@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$to = '<yowonyoon@gmail.com>'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Coda message:  $name";
+$email_body = "You have received a new message from your Coda blog's email form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@coda.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $yowonyoon@gmail.com";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
